b9415e3c7b2b810b6ce03049747f443d0a660e57
Matt Hough <matt@hough.tv>
2017-12-30 17:53:42 +1000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(x, y) {
+  return x + y
+}
+
+function subtract(x, y) {
+  return x - y
+}
+
+function multiply(x, y) {
+  return x * y
+}
+
+function divide(x, y) {
+  return x / y
+}
+
+function inc(n) {
+  n++
+  return n
+}
+
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(stringInt) {
+  return parseInt(stringInt, 10)
+}
+
+function preserveDecimal(stringFloat) {
+  return parseFloat(stringFloat)
+}
